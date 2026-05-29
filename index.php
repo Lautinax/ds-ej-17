4c80370fa9e059cfa66054a2f8adf495cf9973b6
Lautinax <lautaroferraro214@gmail.com>
2026-05-29 20:24:58 -0300

    subir

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'modelo/persona.php';
+require_once 'modelo/direccion.php';
+
+
+$d = new Direccion();
+$d->Calle = 'Ituzaingo';
+$d->Numero = '166';
+$d->Ciudad = 'Rosario';
+
+
+$p = new Persona();
+$p->Nombre = 'Lauty';
+$p->Apellido = 'Ferraro';
+$p->Documento = '445524155';
+$p->Direccion = $d;
+
+$p->ImprimirInformacion();
+
+
